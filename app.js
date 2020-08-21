e4b1503b00bdf1c6aff9f8cc16d5989ce5b67571
Davi silva <davisilva@unochapeco.edu.br>
2020-08-20 23:05:26 -0300

    Criado rota de checklist,e usado a mesma como middleware no app.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const express = require('express')
-
+const express = require('express');
+const checkListRouter = require('./src/routes/checklist')
 const app = express()
 
+//para eu poder passar no corpo da requisição dados json,(midleware)
+app.use(express.json())
+
+app.use('/checklists',checkListRouter)
+
+app.get('/json',(req,res)=>{
+    console.log(req.body)
+    res.json({title:'Tarefa X',done:true})
+})
+
+
+app.listen(3000,()=>{
+    console.log('Servidor foi iniciado')
+})
