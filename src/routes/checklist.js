ac417a1348f925980930a50c1b775c3515fa57cc
Davi silva <davisilva@unochapeco.edu.br>
2020-08-21 00:07:03 -0300

    criado todas as rotas de checkList

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,5 +7,25 @@ router.get('/',(req,res)=>{
     res.send()
 })
 
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    res.status(200).send(req.body)
+
+})
+
+router.get('/:id',(req,res)=>{
+  console.log(req.params.id)
+  res.send(`Id:${req.params.id}`)
+})
+
+router.put('/:id',(req,res)=>{
+    console.log(req.params.id)
+    res.send(`PUT id:${req.params.id}`)
+})
+
+router.delete('/:id',(req,res)=>{
+    console.log(req.params.id)
+    res.send(`delete id:${req.params.id}`)
+})
 
 module.exports= router;
